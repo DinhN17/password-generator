123495103236e9ef4dbd95012ecafe610952e716
Dinhn17 <nquydinh@yahoo.com>
2023-12-22 15:14:00 +1030

    init file script.js and added pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,44 @@
 // Assignment code here
+function generatePassword() {
+  //Object password;
+  var password = {
+    //Char Set
+    lowercaseSet: "abcdefghijklmnopqrstuvwxyz",
+    uppercaseSet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbericSet: "0123456789",
+    specialCharSet: " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+    criteria: passwordCriteria,
+    setCriteria: function(criteriaObj){
+      this.criteria = criteriaObj;
+    }
+  }
 
+  //password criteria object
+  var passwordCriteria = {
+    length: 8,
+    isLowercase: true,
+    isUppercase: true,
+    isNumeric: true,
+    isSpecialCharacters: true,
+    validate: function(){
+      if((length>8 && length<128) && 
+      (isLowercase || isUppercase || isNumeric || isSpecialCharacters)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  //get criteria from user
+
+  //validate criteria: 8 < length < 128 characters and at least one character type.
+
+  //generate the random password
+
+  //display password
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
