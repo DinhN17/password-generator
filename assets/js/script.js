a122eefe892797bd499299db5898b09c78396492
Dinhn17 <nquydinh@yahoo.com>
2023-12-29 15:37:16 +1030

    optimise validatePassword, comment and clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,37 @@
 // Assignment code here
 function generatePassword() {
-  //Object password;
+
+  //Object: passwordCriteria : store criteria and validate criteria
+  var passwordCriteria = {
+    length: 8,
+    includeLowercase: true,
+    includeUppercase: true,
+    includeNumeric: true,
+    includeSpecialCharacters: true,
+    
+    // validate method check if the input of length is > 8 and < 128
+    validateInputLength: function(){
+      if (this.length>=8 && this.length<=128) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    
+    // validate method check if at least one character type should be selected
+    validateCharSet: function(){
+      if(this.includeLowercase ||
+        this.includeUppercase ||
+        this.includeNumeric ||
+        this.includeSpecialCharacters) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  //Object: password : store password and create password which satifies the criteria.
   var password = {
     //Char Set
     lowercaseSet: "abcdefghijklmnopqrstuvwxyz",
@@ -10,110 +41,62 @@ function generatePassword() {
     criteria: passwordCriteria,
     passwd: "",
 
-    //Create password with the given criteria.
+    //Create password with this.criteria.
     createPassword: function() {
-      // create a string which includes all of character satisfies the criteria.
+      
+      // create a string (charSet) which includes all of character satisfies the criteria.
       var charSet = "";
       if (this.criteria.includeLowercase) {
         charSet = charSet + this.lowercaseSet;        
-      }
+      };
       if (this.criteria.includeNumeric) {
         charSet = charSet + this.numbericSet;        
-      }
+      };
       if (this.criteria.includeSpecialCharacters) {
         charSet = charSet + this.specialCharSet;        
-      }
+      };
       if (this.criteria.includeUppercase) {
         charSet = charSet + this.uppercaseSet;        
-      }
+      };
       
-      console.log(charSet);
-
-      //make password from random character of charSet.
+      //create passwd with length and chars from random character of charSet,
+      // and only return the passwd which is validated by the criteria.
       do {
         this.passwd = "";
-        console.log("genPass");
         for (let index = 0; index < this.criteria.length; index++) {
           const element = charSet[index];
           this.passwd += charSet.charAt(Math.floor(Math.random()*charSet.length));
-        };
-        console.log(this.passwd);
-      }while (!this.validatePassword());
-      console.log(this.passwd);
+        }
+      } while (!this.validatePassword());
+
       return this.passwd;
     },
 
-    // Validate if the password satisfy the given criteria.
+    // Validate method check if the passwd satisfies this.criteria by using regex test.
     validatePassword: function(){
-      if (this.passwd.length < 8 || this.passwd.length > 128) {
-        return false
-      };
       
-      if (this.criteria.includeNumeric) {
-        if (!(/\d/.test(this.passwd))) { 
-          return false;
-        };
+      // Check if the passwword satisfies the given criteria.
+      if ((this.criteria.includeLowercase && (!/[a-z]/.test(this.passwd))) ||
+          (this.criteria.includeNumeric && (!/\d/.test(this.passwd))) ||
+          (this.criteria.includeSpecialCharacters && (!/[~!@#$%^&*()_+<>•`{}]/.test(this.passwd))) ||
+          (this.criteria.includeUppercase && (!/[A-Z]/.test(this.passwd)))
+          ) {
+            return false;        
+      } else {
+        return true;        
       };
-
-      if (this.criteria.includeLowercase) {
-        if (!/[a-z]/.test(this.passwd)) {
-          return false;          
-        }        
-      };
-
-      if (this.criteria.includeUppercase) {
-        if (!/[A-Z]/.test(this.passwd)) {
-          return false;          
-        }        
-      };
-
-      if (this.criteria.includeSpecialCharacters) {
-        var regexp = /[~!@#$%^&*()_+<>•`{}]/;
-        if (!(regexp.test(this.passwd))) {
-          return false;          
-        }        
-      }
-
-     
-      return true; 
     }
   }
 
-  //password criteria object
-  var passwordCriteria = {
-    length: 8,
-    includeLowercase: true,
-    includeUppercase: true,
-    includeNumeric: true,
-    includeSpecialCharacters: true,
-    
-    // validate if 8 < length < 128
-    validateLength: function(){
-      if (this.length>=8 && this.length<=128) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    
-    // validate if at least one character type should be selected
-    validateCharSet: function(){
-      if(this.includeLowercase || this.includeUppercase || this.includeNumeric || this.includeSpecialCharacters) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-
-  //get criteria from user
+  // get criteria from user
   alert("Please choose criteria for password : length, lowercase, uppercase, numeric, and/or special characters ");
-  //get length of password and validate if 8 < length < 128 characters
+  
+  // get length of password and validate if 8 < length < 128 characters
   do {
     passwordCriteria.length = Number(prompt("Please set the length of password. It should be between 8 and 128 characters", ""));
-  } while (!passwordCriteria.validateLength());
+  } while (!passwordCriteria.validateInputLength());
 
-  //confirm if password include lowercase, uppercase, special characters and validate if at least one character type was chosen.
+  // confirm if password include lowercase, uppercase, special characters and validate if at least one character type was chosen.
   do {
     alert("Please choose at least one type: lowercase, uppercase, numeric and/or special charaters.");
     passwordCriteria.includeLowercase = confirm("Do you want to include lowercase?");
@@ -121,12 +104,12 @@ function generatePassword() {
     passwordCriteria.includeNumeric = confirm("Do you want to include number?");
     passwordCriteria.includeSpecialCharacters = confirm("Do you want to include special charaters?");
   } while (!passwordCriteria.validateCharSet());
-  // console.log(passwordCriteria);
 
-  //set criteria for password
+  
+  // set criteria for password
   password.criteria = passwordCriteria;
   
-  //return password 
+  // return password 
   return password.createPassword();
 };
 
